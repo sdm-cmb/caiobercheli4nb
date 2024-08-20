3abd8616231ef6d4fc9dc18b00c50daa71f8f24e
sdm-cmb <59738106+sdm-cmb@users.noreply.github.com>
2024-08-20 19:47:08 -0300

    att

--- src/models/userModel.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    id: number;
-    name: string;
-    email: string;
-  }
-  
-  export const users: User[] = [];
-
-  
--- src/routes/userRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getUsers, createUser } from '../controllers/userControllers';
-
-const router = Router();
-
-router.get('/', getUsers);
-router.post('/', createUser);
-
-export default router;
